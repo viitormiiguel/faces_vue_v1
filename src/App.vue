f5320e127a4f348bbcf81969205f16423b72ced9
viitormiiguel <vitor.peres@acad.pucrs.br>
2020-09-08 02:09:20 -0300

    novas functions

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar />    
+    <Reaction />
+    <HelloWorld/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+ import HelloWorld from './components/HelloWorld.vue'
+  import Navbar from './components/Navbar.vue';  
+  import Reaction from './components/Reaction.vue';  
+  export default {
+    name: 'navbar',
+    components: {
+      Navbar,
+      HelloWorld,
+      Reaction
+    }
   }
-}
 </script>
 
 <style>
@@ -23,6 +28,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
---
+++ src/components/Reaction.vue
@@ -0,0 +1,14 @@
+<template>
+    <b-container class="bv-example-row">
+        <b-row>
+            <b-col cols="7">
+                <img class="img-fluid rounded mb-4 mb-lg-0" src="http://placehold.it/900x400" alt="">
+            </b-col>
+            <b-col cols="7">
+                <h1 class="font-weight-light">Business Name or Tagline</h1>
+                <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it makes a great use of the standard Bootstrap core components. Feel free to use this template for any project you want!</p>
+                <a class="btn btn-primary" href="#">Call to Action!</a>
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
